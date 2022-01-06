419ed2497032375dd0de72e9644c108005094d98
SansNumbers <kreig.antipin@gmail.com>
2022-01-06 12:14:20 +0200

    123

--- app/controllers/sign_in_user_controller.rb
+++ app/controllers/sign_in_user_controller.rb
@@ -3,6 +3,9 @@ class SignInUserController < ApplicationController
 
   def create
     user = User.find_by(email: params[:email])
+    puts '------------'
+    puts session[:user_id]
+    puts '------------'
     if user.present? && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to user_page_path(user.id), notice: 'Logged in successfully'
--- app/controllers/sign_up_user_controller.rb
+++ app/controllers/sign_up_user_controller.rb
@@ -5,6 +5,7 @@ class SignUpUserController < ApplicationController
 
   def create
     @user = User.new(user_params)
+
     if @user.save
       session[:user_id] = @user.id
       UserMailer.with(user: @user).welcome_email_user.deliver_now
@@ -20,20 +21,21 @@ class SignUpUserController < ApplicationController
   end
 
   def edit
-    puts 'here'
-    puts session[:user_id]
-    puts 'here'
+
     @problems = Problem.all
     @user = User.find_signed!(params[:token], purpose: 'sign_up_user_verify')
-    puts 'here'
-    puts session[:user_id]
-    puts 'here'
+
   rescue ActiveSupport::MessageVerifier::InvalidSignature
     redirect_to sign_up_user_verification_path, alert: 'Your token has expired. Please try again.'
   end
 
   def update
-    @user = User.find_by(id: session[:user_id]) if session[:user_id]
+
+    puts 'here'
+    puts session[:user_id]
+    puts 'here'
+
+    @user = User.find_signed!(params[:token], purpose: 'sign_up_user_verify')
     if @user.update(updated_params)
       params[:user][:problems]&.each do |problem|
         @user.problems << Problem.find_by(title: problem)
